b272284cc0cb7a93fb2341ea805f281132c43043
donperro05 <santianqr@gmail.com>
2023-09-07 09:13:39 -0500

    move-between-sections

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,13 @@
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Header() {
+  const [activeSection, setActiveSection] = useState("Home");
+  useState("Home");
+  
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -17,15 +22,26 @@ export default function Header() {
           {links.map((link) => (
             <motion.li
               key={link.hash}
-              className="h-3/4 flex items-center justify-center relative"
+              className="h-3/4 flex items-center justify-center relative "
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 href={link.hash}
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                onClick={() => setActiveSection(link.name)}
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition text-gray-950",
+                  { "text-gray-950": activeSection === link.name }
+                )}
               >
                 {link.name}
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{ type: "spring", stiffness: 380, damping: 30 }}
+                  ></motion.span>
+                )}
               </Link>
             </motion.li>
           ))}
